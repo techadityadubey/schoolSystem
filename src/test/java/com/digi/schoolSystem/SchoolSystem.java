0c96d28e1d7f0d35f9e991cd5a259f3b13c1fc17
aditya dubey <tech.adityadubey@gmail.com>
2020-02-02 22:01:12 +0530

    Chnages in Branch File

--- src/test/java/com/digi/schoolSystem/SchoolSystem.java
+++ src/test/java/com/digi/schoolSystem/SchoolSystem.java
@@ -1,4 +1,4 @@
-package com.digi.uniproof;
+package com.digi.schoolSystem;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class UniproofApplicationTests {
+public class SchoolSystem {
 
 	@Test
 	public void contextLoads() {
